5e89b0db5771932f4e7d862ab752d7e00745192e
chandubatta <battachandu996699@gmail.com>
2024-08-19 10:59:10 +0530

    coo

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,22 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Variables in Golang")
+	fmt.Println("Variables and constant")
+	var name string = "chandu"
+	var age = 24
+	married_status := false
+	const quality = "good"
+	fmt.Printf("variable type is %T and value is %v : \n", name, name)
+	fmt.Printf("variable type is %T and value is %v \n", age, age)
+	fmt.Printf("variable type is %T and value is %v \n:", married_status, married_status)
+	fmt.Printf("variable type is %T and value is %v \n:", quality, quality)
 
-	// using var key word
-	var name string
-	name = "chandu"
-	fmt.Printf("Name is %v and Type is %T\n ", name, name)
-	var age int = 24
-	fmt.Printf("Age is %v and Type is %T \n", age, age)
-
-	//implicit type ( with out using of data type) LEXER done the job
-	var married = false
-	fmt.Printf("married %v and type is %T\n", married, married)
-
-	// using valurs operator and with out using var key word
-	height := 5.2
-	weight := 61
-	fmt.Printf("Height is %v and Type is %T\n", height, height)
-	fmt.Printf("Weight is %v and Type is %T\n", weight, weight)
 }
